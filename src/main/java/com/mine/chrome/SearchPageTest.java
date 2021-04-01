5733ce4d39f2e764c3698b79760ca59b2e8f6c8e
Mahesh.Mahadeva <yelekeri@gmail.com>
2021-04-01 13:05:54 -0400

    added headless options

--- src/main/java/com/mine/chrome/SearchPageTest.java
+++ src/main/java/com/mine/chrome/SearchPageTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
@@ -18,7 +19,9 @@ public class SearchPageTest {
     @BeforeClass
     public void beforeClass() {
     	System.setProperty("webdriver.chrome.driver","C:\\MyInstalls\\Selenium\\chromedriver.exe");
-        driver = new ChromeDriver();
+    	ChromeOptions chromeOptions = new ChromeOptions();
+    	chromeOptions.addArguments("--headless");
+        driver = new ChromeDriver(chromeOptions);
     }
 
     @AfterClass
--- src/main/java/com/mine/firefox/TitleTest.java
+++ src/main/java/com/mine/firefox/TitleTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
@@ -18,7 +19,9 @@ public class TitleTest {
     @BeforeClass
     public void beforeClass() {
     	System.setProperty("webdriver.gecko.driver","C:\\MyInstalls\\Selenium\\geckodriver.exe");
-        driver = new FirefoxDriver();
+    	FirefoxOptions options = new FirefoxOptions(); 
+    	options.setHeadless(true);
+        driver = new FirefoxDriver(options);
     }
 
     @AfterClass
